41f8310e39942cb00c89e75ea02b4488b402c784
Daniel Dantas <112111627+DanielDantas16@users.noreply.github.com>
2025-02-27 20:12:24 -0300

    form
    formulario

---
+++ WFinicial/C-Revisao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+
+namespace C_Revisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a primeira nota");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado!!");
+
+            }
+            else if (media <= 5)
+            {
+                Console.WriteLine("Reprovado!!");
+
+            }
+            else
+            {
+                Console.WriteLine("Recuperação");
+            }
+        }
+    }
+}
