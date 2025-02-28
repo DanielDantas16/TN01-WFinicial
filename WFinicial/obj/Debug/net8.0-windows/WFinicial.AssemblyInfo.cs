80cf7d3da0512082203846cca3b01549234c07fd
DANIEL GUIMARAES DANTAS DE ALMEIDA <daniel.gdalmeida@senacsp.edu.br>
2025-02-27 22:21:47 -0300

    Ex3 Atualizado

--- WFinicial/obj/Debug/net8.0-windows/WFinicial.AssemblyInfo.cs
+++ WFinicial/obj/Debug/net8.0-windows/WFinicial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WFInicial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+952a6e227ba5d500677a02212f68c271f5daf780")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d3b509d3dc71a8e2cc3f8ad791c13e89da49b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WFInicial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WFInicial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
